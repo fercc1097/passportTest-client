1a6c2a16a2e4698c9dae020f3ccffaa3e30f6263
fercc1097 <fercc1097@gmail.com>
2020-09-04 23:14:05 -0500

    mongoose connected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
+
 
 function App() {
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+
+  const register = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/register",
+    }).then((res) => console.log(res));
+
+  };
+  const login = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/login",
+    }).then((res) => console.log(res));
+  };
+  const getUser = () => {Axios({
+    method: "GET",
+    withCredentials: true,
+    url: "http://localhost:4000/user",
+  }).then((res) => console.log(res));};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+       <h1>Register</h1> 
+       <input placeholder="username" onChange = {e => setRegisterUsername(e.target.value)}></input>
+       <input placeholder="password" onChange = {e => setRegisterPassword(e.target.value)}></input> 
+       <button onClick={register}> Submit </button>
+      </div>
+      
+      <div class="container">
+       <h1>Login</h1> 
+       <input placeholder="username" onChange = {e => setLoginUsername(e.target.value)}></input> 
+       <input placeholder="password" onChange = {e => setLoginPassword(e.target.value)}></input> 
+       <button onClick={login}> Submit </button>
+      </div>
+      
+      <div class="container">
+       <h1>Get User</h1>  
+       <button onClick = {getUser}> Submit </button>
+      </div>
+      
     </div>
   );
 }
